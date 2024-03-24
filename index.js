8cc699158a8d93a87d5606476da66ab8b847cdc0
rukwarocynthia4093@gmail.com <rukwarocynthia4093@gmail.com>
2024-03-24 22:17:19 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, gift) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+}
+function countDown(num) {
+    if (typeof num !== 'number' || num <= 0 || !Number.isInteger(num)) {
+        console.log("Please provide a positive integer.");
+        return;
+    }
+
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
